43eddccddf8bc8315337caaf50d21c5a5da7a784
Stijn Deschuymer <stijn.deschuymer@showpad.com>
2016-03-05 10:07:03 +0100

    use src folder as default working directory and lib folder as dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const webpack = require('webpack');
 
 module.exports = {
-    context: __dirname + '/lib',
+    context: __dirname + '/src',
     entry: {
         bundle: './index.js',
     },
     output: {
         filename: '[name].js',
         sourceMapFilename: '[name].js.map',
-        path: __dirname + '/dist',
+        path: __dirname + '/lib',
     },
     devtool: 'source-map',
     plugins: [
